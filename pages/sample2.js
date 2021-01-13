ba215d6af6c348935afd99394902f446752aac35
Miclain Keffeler <mickeffeler@gmail.com>
2021-01-13 17:08:02 -0600

    more work

--- pages/sample2.js
+++ pages/sample2.js
@@ -1,9 +1,10 @@
 import { ArcherContainer, ArcherElement } from 'react-archer'
-import Draggable from 'react-draggable'
+import Draggable, {DraggableCore} from 'react-draggable'; // Both at the same time
 import React from 'react'
+
 const rootStyle = { display: 'flex', justifyContent: 'center' }
 const rowStyle = { margin: '200px 0', display: 'flex', justifyContent: 'space-between' }
-const boxStyle = { padding: '10px', border: '1px solid black' }
+const boxStyle = { padding: '10px', border: '1px solid black'  }
 
 export default function Sample1 () {
   const inputEl = React.useRef(null)
@@ -11,77 +12,80 @@ export default function Sample1 () {
     <div style={{ height: '500px', margin: '50px' }}>
       <ArcherContainer ref={inputEl} strokeColor='red'>
         <Draggable onDrag={inputEl.current ? inputEl.current.refreshScreen : null}>
-        <div>
-          <div style={rootStyle}>
-
-          <ArcherElement
-            id='root'
-            relations={[
-              {
-                targetId: 'element2',
-                targetAnchor: 'top',
-                sourceAnchor: 'bottom',
-                style: { strokeDasharray: '5,5' }
-              }
-            ]}
-          >
-            <div style={boxStyle}>System 1</div>
-          </ArcherElement>
-        </div>
-        </div>
-      </Draggable>
-        <Draggable onDrag={inputEl.current ? inputEl.current.refreshScreen : null} style={rowStyle}>
           <div>
+            <div style={rootStyle}>
 
+              <ArcherElement
+                id='root'
+                relations={[
+                  {
+                    targetId: 'element2',
+                    targetAnchor: 'top',
+                    sourceAnchor: 'bottom',
+                    style: { strokeDasharray: '5,5' }
+                  }
+                ]}
+              >
+                <div style={boxStyle}>System 1</div>
+              </ArcherElement>
+            </div>
+          </div>
+        </Draggable>
+        
+        <Draggable onDrag={inputEl.current ? inputEl.current.refreshScreen : null}>
           <div style={rowStyle}>
-          <ArcherElement
-            id='element2'
-            relations={[
-              {
-                targetId: 'element3',
-                targetAnchor: 'left',
-                sourceAnchor: 'right',
-                style: { strokeColor: 'blue', strokeWidth: 1 },
-                label: <div style={{ marginTop: '-20px' }}>Arrow 2</div>
-              }
-            ]}
-          >
-            <div style={boxStyle}>System 2</div>
-          </ArcherElement>
-        </div>
-        </div>
+
+            <div>
+              <ArcherElement
+                id='element2'
+                relations={[
+                  {
+                    targetId: 'element3',
+                    targetAnchor: 'left',
+                    sourceAnchor: 'right',
+                    style: { strokeColor: 'blue', strokeWidth: 1 },
+                    label: <div style={{ marginTop: '-20px' }}>Arrow 2</div>
+                  }
+                ]}
+              >
+                <div style={boxStyle}>System 2</div>
+              </ArcherElement>
+            </div>
+          </div>
         </Draggable>
 
-        <Draggable onDrag={inputEl.current ? inputEl.current.refreshScreen : null} style={rowStyle}>
+        <Draggable onDrag={inputEl.current ? inputEl.current.refreshScreen : null}>
+          <div >
             <div>
 
-          <ArcherElement id='element3'>
-            <div style={boxStyle}>System 3</div>
-          </ArcherElement>
-        </div>
-          </Draggable>
+              <ArcherElement id='element3'>
+                <div style={boxStyle}>System 3</div>
+              </ArcherElement>
+            </div>
+          </div>
+        </Draggable>
 
-        <Draggable onDrag={inputEl.current ? inputEl.current.refreshScreen : null} style={rowStyle}>
-            <div>
+        <Draggable onDrag={inputEl.current ? inputEl.current.refreshScreen : null}>
+          <div style={rootStyle}>
+            <div >
 
-          <ArcherElement
-            id='element4'
-            relations={[
-              {
-                targetId: 'root',
-                targetAnchor: 'right',
-                sourceAnchor: 'left',
-                label: 'Arrow 3'
-              }
-            ]}
-          >
-            <div style={boxStyle}>System 4</div>
-          </ArcherElement>
-        </div>
+              <ArcherElement
+                id='element4'
+                relations={[
+                  {
+                    targetId: 'root',
+                    targetAnchor: 'right',
+                    sourceAnchor: 'left',
+                    label: 'Arrow 3'
+                  }
+                ]}
+              >
+                <div style={boxStyle}>System 4</div>
+              </ArcherElement>
+            </div>
+          </div>
 
-          </Draggable>
-
-
+        </Draggable>
       </ArcherContainer>
     </div>
   )
